d94679fa4325a4bcace8a3a552390d5016a3a66f
ema-0528 <tahminaimo095774@gmail.com>
2026-02-27 08:04:28 +0600

    first commit

---
+++ for-of.js
@@ -0,0 +1,51 @@
+// const number = [23, 56, 467, 89,34,78 , 56];
+// console.log(number);
+// for(const num of number){
+// console.log(num);
+// }
+
+// const number = 7;
+// if( number < 1){
+//     console.log('coto ekta number paichi')
+// }
+// console.log('somthing is new cooking');
+// // ...while loop...
+// let num = 0;
+// while( num < 5){
+//     console.log('looping', num);
+//     num = num + 1;
+//     num += 1;
+//     num++;
+
+// }
+
+// let num = 1;
+// while(num < 10){
+//     console.log(num);
+//     num++;
+// }
+
+
+// let num = 50;
+// while(num <=100){
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 1;
+// let sum = 0;
+// while(num <= 10){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log('sum', sum);
+//     num++;
+// }
+let num = 1;
+while(num <= 10){
+    console.log(num);
+if(num %2 === 0){
+    console.log('even', num);
+}
+    
+    num++;
+}
