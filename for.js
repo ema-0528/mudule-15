4e904b8df1e65a2af5fe93a784ca80f183f44ef7
ema-0528 <tahminaimo095774@gmail.com>
2026-02-27 08:33:50 +0600

    add for

---
+++ for.js
@@ -0,0 +1,26 @@
+//while loop//
+
+// let num = 0; //loop variable
+// while(num < 5){ //loop condition
+//     console.log(num); //loop statement
+//     num++; // loop variable change
+// }
+
+//for loop//
+//for(first part ; second part ; third part){
+// loop statement}
+
+// for( loop variable declaration ; loop condition ; loop variable change ){
+// loop statement
+// }
+
+// let num = 0; //loop variable
+// while(num < 5){ //loop condition
+//     console.log(num); //loop statement
+//     num++; // loop variable change
+// }
+
+for( let num = 0; num < 5; num++){
+    console.log(num);
+}
+// *..jotokkhon sotto na hy totokkhon loop colbe*
