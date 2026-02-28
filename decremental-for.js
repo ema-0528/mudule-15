ba8e1ca9889193134f04c3cfb5f6a1079efe5270
ema-0528 <tahminaimo095774@gmail.com>
2026-02-28 09:00:33 +0600

    add decremental

---
+++ decremental-for.js
@@ -0,0 +1,28 @@
+// incremental loop//
+
+for(let i = 0; i <=10; i++){
+    console.log(i);
+}
+
+// decremental loop// (control c marle bondo hbe)
+// for(let i = 10; i >=0; i--){
+//     console.log(i)
+// }
+
+// for(let i = 20; i >=11; i--){
+//     console.log(i);
+// }
+
+// decremental-while-loop//
+
+// let n = 10;
+// while(n < 0){
+//     console.log(n);
+//     n--;
+// }
+
+let i = 20;
+while(i < 10){
+    console.log(i);
+    i--;
+}
