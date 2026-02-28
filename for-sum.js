5bdb5d1b8e0d60cbd33329a005542b1ea390bf01
ema-0528 <tahminaimo095774@gmail.com>
2026-02-28 08:35:08 +0600

    add while

---
+++ for-sum.js
@@ -0,0 +1,16 @@
+let i = 11;
+let sum = 0;
+for(let i = 11; i <=20; i++){
+    sum = sum + i;
+    console.log(i);
+}
+console.log('some of numbers from 11 to 20 is', sum)
+
+// let num = 1;
+// let sum = 0;
+// while(num <= 10){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log('sum', sum);
+//     num++;
+// }
